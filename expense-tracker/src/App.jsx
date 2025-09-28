0163bbd58e3e5dd8445246de43108ed294d8a2cc
Dinesh Sonesha <dineshsonesha@gmail.com>
2025-09-28 14:22:57 +0530

    redeploy

--- expense-tracker/src/App.jsx
+++ expense-tracker/src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Index from './Index';
 import Dashboard from './pages/Dashboard';
 
 export default function App() {
   return (
     <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
+      <Route path="/" element={<Index />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
   );
 }
